110818952b62babc1b46e03e0ce24176843b66c9
Vasyl <viper.kspu@gmail.com>
2022-02-21 00:20:03 +0200

    recent is writed to DB and displaied link

--- MongoController.js
+++ MongoController.js
@@ -1,7 +1,7 @@
 const MongoClient = require("mongodb").MongoClient;
 const url = "mongodb://localhost:27017/";
 
-var default_user_obj = {id: null, description: "empty user."}
+var default_user_obj = {id: null, description: "empty user.", recents: {path: null, source: null, time: null, timestamp: null}};
 
 class MongoController {
 	constructor() { }
@@ -16,7 +16,6 @@ class MongoController {
             var document = await collection.findOne({}, {id: user_id});
             if (document) {
                 result = document;
-                console.log(`Inside collection users: ${document}`);
             }
             else {
                 console.log(`Fail to find user: ${user_id}`);
@@ -33,35 +32,35 @@ class MongoController {
         var result;
         const mongoClient = new MongoClient(url);
         try {
-            var document = this.getUser(user_id);
+            var document = await this.getUser(user_id);
             if (document) {
                 result = document;
             }
             else {
                 default_user_obj.id = user_id;
-
                 await mongoClient.connect();
                 const db = mongoClient.db("mediaservice");
                 const collection = db.collection("users");
                 await collection.insertOne(default_user_obj);
-                result = this.getUser(user_id);
+                result = await this.getUser(user_id);
             }
-            console.log(`User: ${document.id} is returned`);
         } catch(err) {
             console.log("Exception: " - err);
         } finally {
             await mongoClient.close();
         }
+        return result;
     }
 
-    async updateRecents(user_id, recents) {
+    async updateRecents(user_id, new_recents) {
         const mongoClient = new MongoClient(url);
         try {
             await mongoClient.connect();
             const db = mongoClient.db("mediaservice");
             const collection = db.collection("users");
-            collection.updateOne({id: user_id}, {$set: {recents: recents}});
-            console.log(`User: ${user_id} updated.`);
+            const options = { upsert: true };
+            var result = await collection.updateOne({id: user_id}, {$set: {recents: new_recents}}, options);
+            //console.log( `${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`);
         } catch(err) {
             console.log("Exception: " - err);
         } finally {
