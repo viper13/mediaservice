810771d4ac778993cc7c8636f38084021c638f5f
Vasyl <viper.kspu@gmail.com>
2022-12-31 02:56:36 +0200

    FE moved to function style

--- mediaserviceview/src/App.js
+++ mediaserviceview/src/App.js
@@ -1,81 +1,68 @@
 import './App.css';
 import React from 'react';
 
-class Header extends React.Component {
-  render() {
-    return (
-      <header>
-        <p><a href="/">Home video player</a></p>
-      </header>);
-  }
+function Header(props)  {
+  return (
+    <header>
+      <p><a href="/">Home video player</a></p>
+    </header>);
 }
 
-class NavigationBar extends React.Component {
-  render() {
-    return (
-      <div id="globalnavigation">
-        <p id="plaing">Plaing: </p>
-        <p id="recent">Last seen: <a id="recent_info" href="/TODO"></a></p>
-          <input type='button' id="prevBtn" onclick={() => this.setState({value: 'X'})} value='Prevoius'></input>
-          <input type='button' id="nextBtn" onclick={() => this.setState({value: 'X'})} value='Next'></input>
-      </div>);
-  }
-}
-
-class VideoPlayer extends React.Component {
-  render() {
-    return (<div id="mediacontent">
-      <video id="video" controls>
-        </video>
+function NavigationBar(props)  {
+  return (
+    <div id="globalnavigation">
+      <p id="plaing">Plaing: </p>
+      <p id="recent">Last seen: <a id="recent_info" href="/TODO"></a></p>
+        <input type='button' id="prevBtn" onclick={() => this.setState({value: 'X'})} value='Prevoius'></input>
+        <input type='button' id="nextBtn" onclick={() => this.setState({value: 'X'})} value='Next'></input>
     </div>);
-  }
 }
 
-class VideoList extends React.Component {
-  playMe() {
-    //TODO
-  }
-  render() {
-    return (<div id="list">
-      <p><input type='button' class='video_source' onclick={() => this.playMe()}></input></p>
-      <p><input type='button' class='video_source' onclick={() => this.playMe()}></input></p>
-      <p><input type='button' class='video_source' onclick={() => this.playMe()}></input></p>
+function VideoPlayer(props)  {
+  return (<div id="mediacontent">
+    <video id="video" controls>
+      </video>
+  </div>);
+}
+
+function playMe() {
+  //TODO
+}
+
+function VideoList(props) {
+  return (<div id="list">
+    <p><input type='button' class='video_source' onclick={() => playMe()}></input></p>
+    <p><input type='button' class='video_source' onclick={() => playMe()}></input></p>
+    <p><input type='button' class='video_source' onclick={() => playMe()}></input></p>
+  </div>);
+}
+
+function PlayerSection(props)  {
+  return (
+    <div id="playersection">
+      <VideoPlayer /> 
+      <VideoList />
     </div>);
-  }
 }
 
-class PlayerSection extends React.Component {
-  render() {
-    return (
-      <div id="playersection">
-        <VideoPlayer /> 
-        <VideoList />
-      </div>);
-  }
+function FoldersList(props) {
+  return (
+    <div id="foldersList">
+      <p>Folder 1</p>
+      <p>Folder 2</p>
+      <p>Folder 3</p>
+    </div>
+  );
 }
 
-class FoldersList extends React.Component {
-  render() {
-    return (
-      <div id="foldersList">
-        <p>Folder 1</p>
-        <p>Folder 2</p>
-        <p>Folder 3</p>
-      </div>
-    );
-  }
-}
-
-class DownloadsList extends React.Component {
-  render() {
-    return (
-      <div id="downloadsList">
-        <p>File 1</p>
-        <p>File 2</p>
-        <p>File 3</p>
-      </div>
-    );
-  }
+function DownloadsList(props) {
+  return (
+    <div id="downloadsList">
+      <p>File 1</p>
+      <p>File 2</p>
+      <p>File 3</p>
+    </div>
+  );
 }
 
 function App() {
